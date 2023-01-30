e0a580eae666c7ac0c06a24820bf62e4364e6af9
Aleksey <lexandreeev@gmail.com>
2023-01-31 00:50:26 +0300

    fixed bug task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,22 +1,23 @@
 ﻿int[] array = new int[123];
 void fillArray(int[] arr)
 {
-  Random r = new Random();
+  int r = 0;
   for (int i = 0; i < 123; i++)
   {
-    arr[i] = r.Next(1, 999);
-    Console.Write("{0}", arr[i]);
+    arr[i] = r+1;
+    r++;
+    Console.Write("{0,4}", arr[i]);
   }
   Console.WriteLine();
 }
 int quantityRange(int[] array)
 {
-  int botton = 10;
-  int top = 99;
+  int A = 10;
+  int B = 99;
   int count = 0;
   for (int i = 0; i < array.Length; i++)
   {
-    if ((array[i] >= botton) && (array[i] <= top))
+    if ((array[i] >= A) && (array[i] <= B))
     {
       count++;
     }
